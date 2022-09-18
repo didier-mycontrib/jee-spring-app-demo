610a0f7bc8a0363786136273be917234b9195df3
DidierDefrance <didier@d-defrance.fr>
2022-09-18 21:23:05 +0200

    my-native-app

---
+++ my-native-app/src/main/java/tp/mynativeapp/MyNativeAppApplication.java
@@ -0,0 +1,14 @@
+package tp.mynativeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNativeAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World! This is a Basic Spring Native Application");
+		SpringApplication.run(MyNativeAppApplication.class, args);
+	}
+
+}
