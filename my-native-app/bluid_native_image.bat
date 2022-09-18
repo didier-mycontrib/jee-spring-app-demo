d7ee3095d870fcf6e114149bfdf146eabd0dfc9a
DidierDefrance <didier@d-defrance.fr>
2022-09-18 22:41:55 +0200

    s_8181

--- my-native-app/bluid_native_image.bat
+++
@@ -1,3 +0,0 @@
-set JAVA_HOME=C:\Program Files\Java\jdk-11.0.12
-set PATH="%JAVA_HOME%\bin";%PATH%
-mvn spring-boot:build-image > resBuild.txt 2>&1
--- my-native-app/src/main/java/tp/mynativeapp/MyNativeAppApplication.java
+++ my-native-app/src/main/java/tp/mynativeapp/MyNativeAppApplication.java
@@ -9,6 +9,7 @@ public class MyNativeAppApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello, World! This is a Basic Spring Native Application");
 		SpringApplication.run(MyNativeAppApplication.class, args);
+		System.out.println("http://localhost:8181/my-native-app");
 	}
 
 }
